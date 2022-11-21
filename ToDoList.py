eac13b112fb6ba01017d841250e5a9391000438b
mosafari <m.safari2680@gmail.com>
2022-11-21 15:33:59 +0330

    add main section

--- ToDoList.py
+++ ToDoList.py
@@ -28,3 +28,9 @@
 
 # disply tasks
 
+
+# main
+
+num = 0 # number of tasks
+task_list = [] # list of tasks
+status ={} # status of the task key =  task and value = status  -> status =(Done, Not Started)
