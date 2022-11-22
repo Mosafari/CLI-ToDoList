8727705e06d480d25fb8bf0799b4c31374dc3825
mosafari <m.safari2680@gmail.com>
2022-11-22 12:08:20 +0330

    'add_task' section complete

--- ToDoList.py
+++ ToDoList.py
@@ -7,7 +7,14 @@
 
 
 # add ToDo
-
+def add_task():
+    task = input("Enter your task : ")
+    if task.isnumeric():
+        print("Task is not valid")
+        add_task()
+    else:
+        task_list.append(task)
+        status[task] = 'Not Started'
 
 
 
@@ -34,3 +41,5 @@
 num = 0 # number of tasks
 task_list = [] # list of tasks
 status ={} # status of the task key =  task and value = status  -> status =(Done, Not Started)
+add_task()
+print(task_list,status)
